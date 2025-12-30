f0b706f5c1c72c8e1a092210f091578b0e8bb73a
SuiXie-ja <wswsdadajkjk@outlook.com>
2025-12-30 16:10:54 +0800

    添加了查看历史记录的功能，在计算一次后可以在同一文件夹找到

--- 项目/Form1.cs
+++ 项目/Form1.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows.Forms;
+using System.IO;
 
 namespace 项目
 {
@@ -11,9 +12,9 @@ namespace 项目
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
+        {   File.AppendAllText("历史记录.txt", $"{DateTime.Now}使用了一次一元二次方程求解功能，参数为：a={textBox1.Text}, b={textBox2.Text}, c={textBox3.Text}\n");
+          try
+          {
                 int a = int.Parse(textBox1.Text), b = int.Parse(textBox2.Text), c = int.Parse(textBox3.Text);
             if (a == 0)
             {
@@ -27,8 +28,9 @@ namespace 项目
                 d = Math.Abs(d);
                 double x2 = (-b - Math.Sqrt(d)) / (2.0 * a);
                 double x1 = (-b + Math.Sqrt(d)) / (2.0 * a);
-                MessageBox.Show($"算式为({b}+√{d})/(2*{a}) 或者({b}-√{d})/(2*{a})\n有两个虚数解：x1={x1}i, x2={x2}i");
-                return;
+                MessageBox.Show($"算式为({b}+√{d})±(2*{a})\n有两个虚数解：x1={x1}i, x2={x2}i");
+                File.AppendAllText("历史记录.txt", $"两个虚数解,方程{a}x^2+{b}x+{c}=0的解为：x1={x1}, x2={x2}\n");
+                    return;
             }
             else
             {
@@ -38,15 +40,19 @@ namespace 项目
             {
                 double x = -b / (2.0 * a);
                 MessageBox.Show($"有唯一解：x={x}，算式为{-b}/(2*{a})");
+               File.AppendAllText("历史记录.txt", $"唯一有解，方程{a}x^2+{b}x+{c}=0的解为：x1={x}\n");
+
             }
-            else
+                else
             {
                 double x1 = (-b + Math.Sqrt(d)) / (2.0 * a);
                 Console.WriteLine(x1);
                 double x2 = (-b - Math.Sqrt(d)) / (2.0 * a);
-                MessageBox.Show($"算式为({b}+√{d})/(2{a}) 或者({b}-√{d})/(2{a})\n有两个解：x1={x1}, x2={x2}");
+                MessageBox.Show($"判别式式为({b}+√{d})±(2{a})\n有两个解：x1={x1}, x2={x2}");
+                File.AppendAllText("历史记录.txt", $"方程{a}x^2+{b}x+{c}=0的解为：x1={x1}, x2={x2}\n");
             }
-            } catch
+          } 
+            catch
             {
                 MessageBox.Show("请输入正确的数字"); return;
             }
